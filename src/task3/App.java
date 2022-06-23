ee485a283fe26a53c265851c8259157641d1407f
Serhii Asanov <serge.asanov@gmail.com>
2022-06-23 22:43:59 +0300

    finishe factorial task

--- src/task3/App.java
+++ src/task3/App.java
@@ -1,11 +1,30 @@
 package task3;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Factorial factorial = new Factorial();
 
+        BigInteger fct = new BigInteger("1");
+        BigInteger number = new BigInteger("100");
+
+        for (BigInteger i = new BigInteger("1"); i.compareTo(number) <= 0; i = i.add(BigInteger.ONE)) {
+            fct = fct.multiply((i));
+        }
+
+        String factorialToString = fct.toString();
+        int[] factorialToInt = new int[160];
+
+        for (int i = 0; i < factorialToString.length(); i++) {
+            factorialToInt[i] = Character.getNumericValue(factorialToString.charAt(i));
+        }
         
-        int sum = factorial.sumOfDigitsFactorial(factorial.findFactorial(20));
-        System.out.println(sum);
+        int sumOfDigits = 0;
+
+        for (int fi : factorialToInt) {
+            sumOfDigits += fi;
+        }
+        System.out.println("Sum of digits factorial = " + sumOfDigits);
     }
 }
