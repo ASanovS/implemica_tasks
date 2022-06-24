afa0dbea357accde5c4fc2ea80367ad22425d7f6
Serhii Asanov <serge.asanov@gmail.com>
2022-06-24 22:15:26 +0300

    Changed factorial calculation loop

--- src/task3/App.java
+++ src/task3/App.java
@@ -11,31 +11,34 @@ import java.math.BigInteger;
 public class App {
     public static void main(String[] args) {
 
-        //Value to find factorial number
-        BigInteger startValueFactorial = new BigInteger("1");
+        // Value to find factorial number
+        BigInteger startValueFactorial = BigInteger.valueOf(1);
+        BigInteger numberOfMultiple = BigInteger.valueOf(1);
+        
+        // The number whose factorial will be calculated
+        BigInteger inputFactorialValue = BigInteger.valueOf(100);
 
-        //The number whose factorial will be calculated
-        BigInteger inputFactorialValue = new BigInteger("100");
-
-        for (BigInteger i = new BigInteger("1"); i.compareTo(inputFactorialValue) <= 0; i = i.add(BigInteger.ONE)) {
-            startValueFactorial = startValueFactorial.multiply((i));
+        while (inputFactorialValue != BigInteger.ZERO) {
+            inputFactorialValue = inputFactorialValue.subtract(BigInteger.ONE);
+            startValueFactorial = startValueFactorial.multiply(numberOfMultiple);
+            numberOfMultiple = numberOfMultiple.add(BigInteger.ONE);
         }
 
         System.out.println("Factorial 100! = " + startValueFactorial);
 
-        //Casting a factorial value to a string type
+        // Casting a factorial value to a string type
         String factorialToString = startValueFactorial.toString();
 
-        //Calculating the length of a string to create an array of Integer
+        // Calculating the length of a string to create an array of Integer
         System.out.println("String length = " + factorialToString.length());
         int[] factorialToInt = new int[158];
 
-        //Filling an array with numbers from a string
+        // Filling an array with numbers from a string
         for (int i = 0; i < factorialToString.length(); i++) {
             factorialToInt[i] = Character.getNumericValue(factorialToString.charAt(i));
         }
 
-        //Calculate the sum of all numbers in an array.
+        // Calculate the sum of all numbers in an array.
         int sumOfDigits = 0;
         for (int fi : factorialToInt) {
             sumOfDigits += fi;
