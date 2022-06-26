7d95af4e0d8d55f239dd3deafe12b9a9f1529ab5
Serhii Asanov <serge.asanov@gmail.com>
2022-06-26 10:29:42 +0300

    task1

--- src/task1/App.java
+++ src/task1/App.java
@@ -1,8 +1,50 @@
 package task1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
-    
     public static void main(String[] args) {
+
+        int count = 1;
+        List<String> current = new ArrayList<>();
+        current.add("()");
         
+        List<String> newList = new ArrayList<>();
+        String temp;
+
+        int countBrackets;
+        while (true) {
+            System.out.println("Enter pairs of brackets number");
+            Scanner in = new Scanner(System.in);
+            if (in.hasNextInt()) {
+                countBrackets = in.nextInt();
+                break;
+            }
+        }
+
+        //Checking the input value for 0
+        if (countBrackets <= 0) {
+            System.out.println("Count of brackets should be Integer and more than 0");
+        }
+
+        while (count < countBrackets) {
+            for (String o : current) {
+                for (int i = 0; i < o.length(); i++) {
+                    // Add "()" before each "(" or ")" and add in list if it is not contain
+                    temp = o.substring(0, i) + "()" + o.substring(i);
+                    if (newList.contains(temp))
+                        continue;
+                    newList.add(temp);
+                }
+            }
+            current = newList;
+            newList = new ArrayList<>();
+            count++;
+        }
+        
+        System.out.println(current.toString());
+        System.out.println("Correct brackets expressions = " + current.size());
     }
 }
